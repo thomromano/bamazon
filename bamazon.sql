be2fb9b4ea15b1568041cd3b0659915c77552906
thomromano <thomromano@gmail.com>
2018-05-07 17:31:50 -0500

    add bamazon.sql

---
+++ bamazon.sql
@@ -0,0 +1,13 @@
+DROP DATABASE if EXISTS Bamazon;
+CREATE DATABASE Bamazon;
+USE Bamazon;
+
+-- Create a table called 'products' to contain the store inventory --
+CREATE TABLE products (
+	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(30) NOT NULL,
+	department_name VARCHAR(20) NOT NULL,
+	price DECIMAL(10,2) NOT NULL,
+	stock_quantity INTEGER(11) NOT NULL,
+	PRIMARY KEY (item_id)
+);
